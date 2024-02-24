55cf3e905489eeb66cc2c4706edd8d9b0813c66d
Dan Munro <dan@danmunro.com>
2024-02-23 20:05:17 -0800

    basic rewrite done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -44,7 +44,7 @@ export default function Home() {
   }
 
   return (
-    <Container>
+    <Container title="Home">
       { !loggedInUser && (
         <PaperContainer>
           <Typography variant="h1">
