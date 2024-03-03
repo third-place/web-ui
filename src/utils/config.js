e4cdeab9e5e39d1cc84d2dcb3ea8b5c7594cedee
Dan Munro <dan@danmunro.com>
2024-03-02 22:06:00 -0800

    continue tweaking dev deployment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
-const userService = process.env.USER_SERVICE || 'http://localhost:9004';
-const communityService = process.env.COMMUNITY_SERVICE || 'http://localhost:9001';
-const imageService = process.env.IMAGE_SERVICE || 'http://localhost:9002';
-const notificationService = process.env.NOTIFICATION_SERVICE || 'http://localhost:9003';
-const imageBaseUrl = process.env.IMAGE_HOST || 'https://thirdplace-local-dev.s3.us-west-2.amazonaws.com';
+const userService = import.meta.env.VITE_USER_SERVICE || 'http://localhost:9004';
+const communityService = import.meta.env.VITE_COMMUNITY_SERVICE || 'http://localhost:9001';
+const imageService = import.meta.env.VITE_IMAGE_SERVICE || 'http://localhost:9002';
+const notificationService = import.meta.env.VITE_NOTIFICATION_SERVICE || 'http://localhost:9003';
+const imageBaseUrl = import.meta.env.VITE_IMAGE_HOST || 'https://thirdplace-local-dev.s3.us-west-2.amazonaws.com';
 const env = 'prod';
 const primaryColor = '#e71d36';
 const secondaryColor = '#17A398';
