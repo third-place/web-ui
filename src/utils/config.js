9136cf6a811b252b7b0b1c6afa7e21ee12efdc19
Dan Munro <dan@danmunro.com>
2024-03-02 19:45:29 -0800

    dev build

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,9 @@
-const userService = 'http://localhost:9004';
-const communityService = 'http://localhost:9001';
-const imageService = 'http://localhost:9002';
-const notificationService = 'http://localhost:9003';
-const imageBaseUrl = 'https://thirdplace-local-dev.s3.us-west-2.amazonaws.com';
+const userService = process.env.USER_SERVICE || 'http://localhost:9004';
+const communityService = process.env.COMMUNITY_SERVICE || 'http://localhost:9001';
+const imageService = process.env.IMAGE_SERVICE || 'http://localhost:9002';
+const notificationService = process.env.NOTIFICATION_SERVICE || 'http://localhost:9003';
+const imageBaseUrl = process.env.IMAGE_HOST || 'https://thirdplace-local-dev.s3.us-west-2.amazonaws.com';
 const env = 'prod';
-const appName = 'Home';
 const primaryColor = '#e71d36';
 const secondaryColor = '#17A398';
 const backgroundColor = '#FDFFFC';
@@ -16,7 +15,6 @@ export {
   notificationService,
   imageBaseUrl,
   env,
-  appName,
   primaryColor,
   secondaryColor,
   backgroundColor,
